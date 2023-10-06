eb7f6741a0865839aec32e5da1ff9251d69e40b6
Victor Stala <victorstalalol@gmail.com>
2023-10-06 09:13:29 +0200

    Tarea

--- DesarrolloWebCliente/Tema1/Relacion1/2dawJS.js
+++ DesarrolloWebCliente/Tema1/Relacion1/2dawJS.js
@@ -33,3 +33,13 @@ function preguntarEdad(){
     var edad = prompt("Introduce tu edad: ")
     document.write("Tu nombre es "+nombre+" y has vivido "+edad*365+" dias.")
 }
+function preguntarNumero(){
+    var numero = prompt("Introcduce un numero: ")
+    alert("El doble del numero: "+numero*2+", el triple: "+numero*3+", el cuadruple: "+numero*4);
+}
+function calcCirculo(){
+    var rad = prompt("Introduce el radio del circulo: ")
+    var longitud = 2*Math.PI*rad;
+    var area = Math.PI*Math.pow(rad, 2);
+    document.write("Longitud: "+longitud+", area: "+area);
+}
