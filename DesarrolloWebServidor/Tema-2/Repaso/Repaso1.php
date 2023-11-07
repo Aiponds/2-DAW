c8491dd7931aa11a646451511d3383342077a2b5
Victor Stala <victorstalalol@gmail.com>
2023-11-07 08:48:10 +0100

    PHP

---
+++ DesarrolloWebServidor/Tema-2/Repaso/Repaso1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ej1</title>
+</head>
+<body>
+    <?php
+        $nom = "Victor";
+        $apell1 = "Stala";
+        $apell2 = "Szatan";
+        $edad = 19;
+        print("Me llamo ");
+    ?>
+</body>
+</html>
