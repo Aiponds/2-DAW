f4a2f9192befc2f89aa34d4270ae80279c916d45
Victor Stala <victorstalalol@gmail.com>
2024-02-20 09:19:46 +0100

    asd

--- DisenoInterfacesWeb/Proyecto-2/js/script.js
+++ DisenoInterfacesWeb/Proyecto-2/js/script.js
@@ -1,11 +1,14 @@
 $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
         $(".back-to-top").fadeIn("slow");
+        document.getElementById("main").focus();
     } else {
         $(".back-to-top").fadeOut("slow");
+        document.getElementById("main").focus();
     }
 });
 $(".back-to-top").click(function () {
     $("html, body").animate({ scrollTop: 0 }, 1500);
+    document.getElementById("main").focus();
     return false;
 });
